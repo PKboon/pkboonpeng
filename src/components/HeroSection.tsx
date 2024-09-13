7f26aaea10709f4ce49c7889d6ba7f4091fd2ff1
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2024-09-13 01:15:12 -0400

    update the rest of the components and index

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,23 +2,21 @@ import { Center, Title } from "@mantine/core";
 
 export function HeroSection() {
 	return (
-		<>
-			<Center className="flex flex-col h-screen mx-auto gap-4 max-w-[18.75rem] sm:max-w-[30.75rem] mt-0 sm:-mt-5 ">
-				<Title
-					order={1}
-					className="text-primary-100 text-center text-5xl sm:text-[5rem]"
-				>
-					PK Boonpeng
+		<Center className="flex flex-col h-screen mx-auto gap-4 max-w-[18.75rem] sm:max-w-[30.75rem] mt-0 sm:-mt-5 ">
+			<Title
+				order={1}
+				className="text-primary-100 text-center text-5xl sm:text-[5rem]"
+			>
+				PK Boonpeng
+			</Title>
+			<div className="w-fit sm:w-full grid sm:flex text-center self-center justify-between text-secondary-400 gap-y-.5">
+				<Title order={2} className="text-lg sm:text-[1.375rem]">
+					Front-end Software Engineer
 				</Title>
-				<div className="w-fit sm:w-full grid sm:flex text-center self-center justify-between text-secondary-400 gap-y-.5">
-					<Title order={2} className="text-lg sm:text-[1.375rem]">
-						Front-end Software Engineer
-					</Title>
-					<Title order={2} className="text-lg sm:text-[1.375rem]">
-						UI/UX Designer
-					</Title>
-				</div>
-			</Center>
-		</>
+				<Title order={2} className="text-lg sm:text-[1.375rem]">
+					UI/UX Designer
+				</Title>
+			</div>
+		</Center>
 	);
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,52 +1,70 @@
-import {
-	ExperienceItem,
-	ExperienceItemProps,
-} from "./ExperienceComponents/ExperienceItem";
-import { ExperienceSection } from "./ExperienceComponents/ExperienceSection";
-import { ProjectItem, ProjectItemProps } from "./ProjectComponents/ProjectItem";
-import { ProjectSection } from "./ProjectComponents/ProjectSection";
-import { Tag, TagProps } from "./ProjectComponents/Tag";
-import {
-	ReferrerCard,
-	ReferrerCardProps,
-} from "./ReferrerComponents/ReferrerCard";
-import { ReferrerSection } from "./ReferrerComponents/ReferrerSection";
+import { LinkObject } from "@/types";
+const socialLinks: LinkObject[] = [
+	{
+		id: "social1",
+		href: "https://github.com/PKboon/",
+		icon: "github",
+		tooltip: "Go to GitHub",
+	},
+	{
+		id: "social2",
+		href: "https://www.linkedin.com/in/pkboon/",
+		icon: "linkedin",
+		tooltip: "Go to LinkedIn",
+	},
+];
+
+import { TopBar } from "./TopBar";
+import { HeroSection } from "./HeroSection";
 import {
 	InfiniteSlidingLoop,
 	InfiniteSlidingLoopProps,
 } from "./SkillComponents/InfiniteSlidingLoop";
 import { SkillChip, SkillChipProps } from "./SkillComponents/SkillChip";
 import { SkillSection } from "./SkillComponents/SkillSection";
-import { Icon, IconProps } from "./Icon";
-import { IconLink, IconLinkProps } from "./IconLink";
-import { HeroSection } from "./HeroSection";
-import { TopBar } from "./TopBar";
+import { Tag, TagProps } from "./ProjectComponents/Tag";
+import { ProjectItem, ProjectItemProps } from "./ProjectComponents/ProjectItem";
+import { ProjectSection } from "./ProjectComponents/ProjectSection";
+import {
+	ExperienceItem,
+	ExperienceItemProps,
+} from "./ExperienceComponents/ExperienceItem";
+import { ExperienceSection } from "./ExperienceComponents/ExperienceSection";
+import {
+	ReferrerCard,
+	ReferrerCardProps,
+} from "./ReferrerComponents/ReferrerCard";
+import { ReferrerSection } from "./ReferrerComponents/ReferrerSection";
 import { FooterSection } from "./FooterSection";
 
+import { Icon, IconProps } from "./Icon";
+import { IconLink, IconLinkProps } from "./IconLink";
+
 export {
-	ExperienceItem,
-	ExperienceSection,
-	ProjectItem,
-	ProjectSection,
-	Tag,
-	ReferrerCard,
-	ReferrerSection,
+	socialLinks,
+	TopBar,
+	HeroSection,
 	InfiniteSlidingLoop,
 	SkillChip,
 	SkillSection,
+	Tag,
+	ProjectItem,
+	ProjectSection,
+	ExperienceItem,
+	ExperienceSection,
+	ReferrerCard,
+	ReferrerSection,
+	FooterSection,
 	Icon,
 	IconLink,
-	HeroSection,
-	TopBar,
-	FooterSection,
 };
 export type {
-	ExperienceItemProps,
-	ProjectItemProps,
-	TagProps,
-	ReferrerCardProps,
 	InfiniteSlidingLoopProps,
 	SkillChipProps,
+	TagProps,
+	ProjectItemProps,
+	ExperienceItemProps,
+	ReferrerCardProps,
 	IconProps,
 	IconLinkProps,
 };
