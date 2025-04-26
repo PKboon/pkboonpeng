736e43001e60747b8155945a8007be64fc81d1a4
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-04-25 20:32:25 -0400

    update experience

--- src/components/ExperienceComponents/ExperienceItem.tsx
+++ src/components/ExperienceComponents/ExperienceItem.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { ExperienceObject } from "@/types";
-import { Image, Text, Timeline, Title } from "@mantine/core";
+import {
+	Badge,
+	Group,
+	Image,
+	List,
+	Text,
+	Timeline,
+	Title,
+} from "@mantine/core";
 
 export type ExperienceItemProps = {
-	children: React.ReactNode;
 	experience: ExperienceObject;
 };
 
-export function ExperienceItem({ children, experience }: ExperienceItemProps) {
+export function ExperienceItem({ experience }: ExperienceItemProps) {
 	return (
 		<Timeline.Item
 			bullet={
@@ -20,34 +27,55 @@ export function ExperienceItem({ children, experience }: ExperienceItemProps) {
 			}
 			className="max-w-[42.25rem] pb-3"
 		>
-			<div
-				className="hover:bg-primary-750 cursor-pointer rounded-2xl px-4 py-2"
-				onClick={() => window.open(`${experience.website}`, "_blank")}
-			>
-				<div className="inline-flex flex-wrap items-baseline gap-x-1.5 gap-y-.5">
-					<Title order={5} className="text-base text-primary-200">
+			<div className="inline-flex flex-wrap items-baseline gap-x-1.5 gap-y-.5">
+				<Title order={5} className="text-base text-primary-200">
+					<a
+						href={experience.website}
+						target="_blank"
+						className="underline"
+					>
 						{experience.company}
-					</Title>
-					{experience.companyNote && (
-						<Text className="font-bold text-base text-primary-200">
-							{experience.companyNote}
-						</Text>
-					)}
-					<Text className="text-sm text-primary-200">
-						{experience.location}
-					</Text>
-				</div>
-				<Title order={3} className="text-2xl text-secondary-400">
-					{experience.position}
+					</a>
 				</Title>
-				<Text className="text-sm opacity-80 text-primary-200 mt-0.5 mb-1">
-					{experience.period}
+				{experience.companyNote && (
+					<Text className="font-bold text-base text-primary-200">
+						{experience.companyNote}
+					</Text>
+				)}
+				<Text className="text-sm text-primary-200">
+					{experience.location}
 				</Text>
-
-				<span className="text-base text-primary-200 opacity-80">
-					{children}
-				</span>
 			</div>
+
+			<Title order={3} className="text-2xl text-secondary-400">
+				{experience.position}
+			</Title>
+
+			<Text className="text-sm opacity-80 text-primary-200 mt-0.5 mb-1">
+				{experience.period}
+			</Text>
+
+			<Group gap={6} className="my-3">
+				{experience.skills.map((skill, index) => (
+					<Badge
+						key={`${experience.id}Skill${index}`}
+						className="bg-transparent text-accent-5 border-accent-5 opacity-80 normal-case text-xs"
+					>
+						{skill}
+					</Badge>
+				))}
+			</Group>
+
+			<List listStyleType="disc">
+				{experience.details.map((detail, index) => (
+					<List.Item
+						key={`${experience.id}Detail${index}`}
+						className="text-base text-primary-200"
+					>
+						{detail}
+					</List.Item>
+				))}
+			</List>
 		</Timeline.Item>
 	);
 }
