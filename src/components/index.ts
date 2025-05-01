a21e6f9b37d32f0e0d323de681396b35dc1a3aba
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-05-01 13:59:46 -0400

    update resume link

--- src/components/index.ts
+++ src/components/index.ts
@@ -14,7 +14,7 @@ export const SOCIAL_LINKS: LinkObject[] = [
 	},
 ];
 export const RESUME_LINK: string =
-	"https://drive.google.com/file/d/1tb2IKsOQ3c9P7y9pALm3IBLQS89Zskjp/view?usp=sharing";
+	"https://drive.google.com/file/d/1ExGPlNba1lfQ9pYhcytzcpMRojkFLg8H/view?usp=sharing";
 
 export * from "./ExperienceComponents";
 export * from "./ProjectComponents";
